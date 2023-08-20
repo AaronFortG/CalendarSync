c0e741aff3a1eb659e97192ae2bc57476915262e
Aaron Fort Garcia <aaron.fort@students.salle.url.edu>
2023-08-20 18:04:43 +0200

    · Get the new added events each 10 seconds.

--- main.py
+++ main.py
@@ -1,61 +1,47 @@
-from __future__ import print_function
-
-import datetime
-import os.path
-
-from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+import time
+from datetime import datetime
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/calendar']  # Create, read and modify events.
+# Set up the Google Calendar API
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+
+def get_credentials():
+    flow = InstalledAppFlow.from_client_secrets_file(
+        'credentials.json', SCOPES)
+    credentials = flow.run_local_server(port=0)
+    return credentials
 
 
 def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+    credentials = get_credentials()
+    service = build('calendar', 'v3', credentials=credentials)
 
-    try:
-        service = build('calendar', 'v3', credentials=creds)
+    print("Listening for real-time updates. Press Ctrl+C to stop.")
+    last_check_time = datetime.utcnow()
 
-        # Call the Calendar API
-        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-        print('Getting the upcoming 10 events')
-        events_result = service.events().list(calendarId='primary', timeMin=now,
-                                              maxResults=10, singleEvents=True,
-                                              orderBy='startTime').execute()
+    while True:
+        print("New update")
+        events_result = service.events().list(
+            calendarId='primary', updatedMin=last_check_time.isoformat() + 'Z').execute()
         events = events_result.get('items', [])
 
-        if not events:
-            print('No upcoming events found.')
-            return
+        if events:
+            for event in events:
+                event_id = event['id']
+                print("New Event ID:", event_id)
+                print("Summary:", event.get('summary', 'N/A'))
+                print("Location:", event.get('location', 'N/A'))
+                print("Start:", event['start'].get('dateTime', event['start'].get('date')))
+                print("End:", event['end'].get('dateTime', event['end'].get('date')))
+                print("Description:", event.get('description', 'N/A'))
+                print("Organizer:", event.get('organizer', {}).get('displayName', 'N/A'))
+                print("-----")
+                last_check_time = datetime.utcnow()  # Update last check time to prevent duplicate event retrieval
 
-        # Prints the start and name of the next 10 events
-        for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            print(start, event['summary'])
-
-    except HttpError as error:
-        print('An error occurred: %s' % error)
+        time.sleep(10)  # Wait for 10 seconds before checking for updates again
 
 
 if __name__ == '__main__':
